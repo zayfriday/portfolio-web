4aa696de376541877fa068483d94642de90b54c2
zayfriday <zayfriday1@gmail.com>
2025-01-13 10:24:37 -0500

    changed favicon

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -29,7 +29,7 @@ function Hero() {
                 <div className='w-full flex flex-col items-center'>
                     <motion.h1
                      initial={{opactiy: 0, x:-100}}
-                     whileInView={{opacity: 1, x: 0}}
+                     animate={{opacity: 1, x: 0}}
                      transition={{ duration: 0.5 }}
                      viewport={{ once: true, scroll: true }}
                      className='text-center pb-12 text-7xl lg:text-9xl font-thin mt-28 lg:tracking-wide text-slate-500'>
@@ -37,7 +37,7 @@ function Hero() {
                     </motion.h1>
                     <motion.span
                      initial={{ x: -300, opactiy: 0 }}
-                     whileInView={{ x: 0, opacity: 1 }}
+                     animate={{ x: 0, opacity: 1 }}
                      transition={{ duration: 1 }}
                      viewport={{ once: true }}
                      className='bg-gradient-to-r from-emerald-400 via-fuchsia-200 to-violet-700
@@ -46,7 +46,7 @@ function Hero() {
                     </motion.span>
                     <motion.div
                         initial={{ x: -300, opactiy: 0 }}
-                        whileInView={{ x: 0, opacity: 1 }}
+                        animate={{ x: 0, opacity: 1 }}
                         transition={{ duration: 1.3 }}
                         viewport={{ once: true }}
                         className=''>
